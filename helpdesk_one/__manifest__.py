77cbc8f773cb6b7303f41621684b54a9e55af195
ahmedashraf <www.ahmedashraf83@gmail.com>
2024-05-18 21:52:54 +0300

    help_desk

---
+++ helpdesk_one/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "help_desk_one",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+
+    """,
+
+    'author': "Ahmed Ashraf",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'version': '15.0.0.1',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','helpdesk',],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'views/helpdesk_ticket_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+
